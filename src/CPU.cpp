7eaec2200e3c164b9bc7c9643c2cb4ed1df25c99
Timofey Vlasenko <94196049+Tivlas@users.noreply.github.com>
2024-02-04 12:51:39 +0300

    Курсач (#1)
    * Генерация тестовых данных.
    
    * Проект с умножением и сложением матриц.
    
    * gitignore
    
    * Убрал работу с txt, чтение из бинарного перенесу в каждый проект.
    
    * Добавил чтение из бинарного.
    
    * Код на ЦПУ.
    
    * Добавил аргументы командной строки.
    
    * добавил аргументы командной строки.
    
    * убрал base_dir.
    
    * Переделал проверку на существование файла.
    
    * Файл создается автоматически.
    
    * Однопоточный аналог.
    
    * Небольшие правки.
    
    * Аналог с <<<1,1>>> настройкой ядра.
    
    * Перенес Headers файлы в соответствующие cu & cpp файлы.
    
    * Незначительные правки.
    
    * Исправил ошибку. В ядрах теперь цикл.
    
    * Добавил код.
    
    * Можно генерить float.
    
    * Теперь можно работать с float.
    
    * Теперь можно работать с float.
    
    * Исправил ошибку генерации float.
    
    * Поменял стандарт плюсов.
    
    * Убрал лишнее.
    
    * Явно указал возвращаемый тип.
    
    * Убрал pragma once.
    
    * Явный каст size_t -> ULL в thrust::transform.
    
    * Схемы.
    
    * Залил исполняемые файлы.
    
    * Оптимизация.
    
    * Перекомпилировал файлы.
    
    * Поменял i и j местами.
    
    * Новый код и результаты.
    
    * на всякий случай.
    
    * Использование разделяемой памяти.
    
    * Схемы и графики.
    
    * Убрал лишний код.
    
    * Cохранил все.
    
    * Оставил только нужное.
    
    * переименовал папку.
    
    * Удалил исполняемые файлы.
    
    * Доработка 5ой главы.
    
    * Доки

---
+++ src/CPU.cpp
@@ -0,0 +1,131 @@
+#include <omp.h>
+
+#include <atomic>
+#include <chrono>
+#include <cmath>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+template <typename T>
+void readFromBinary(std::vector<T>& v, const std::filesystem::path& fileName) {
+    std::ifstream file(fileName, std::ios::binary);
+    if (!file.good())
+        throw std::invalid_argument("Invalid file! " + fileName.string());
+    size_t size = 0;
+    file.read(reinterpret_cast<char*>(&size), sizeof(size_t));
+    v.resize(size);
+    file.read(reinterpret_cast<char*>(v.data()), size * sizeof(T));
+    file.close();
+}
+
+template <typename T>
+void matrixMul(const std::vector<T>& a, const std::vector<T>& b,
+               std::vector<T>& result) {
+    size_t size = sqrt(a.size());
+    int j, k;
+#pragma omp parallel for
+    for (int i = 0; i < size; i++) {
+        for (k = 0; k < size; k++) {
+            for (j = 0; j < size; j++) {
+                result[i * size + j] += a[i * size + k] * b[k * size + j];
+            }
+        }
+    }
+}
+
+template <typename T>
+void matrixMulNotParallel(const std::vector<T>& a, const std::vector<T>& b,
+                          std::vector<T>& result) {
+    size_t size = sqrt(a.size());
+    int j, k;
+    for (int i = 0; i < size; i++) {
+        for (k = 0; k < size; k++) {
+            for (j = 0; j < size; j++) {
+                result[i * size + j] += a[i * size + k] * b[k * size + j];
+            }
+        }
+    }
+}
+
+void printProgressIndicator(std::atomic<bool>& isCalculating) {
+    const std::string indicators = "-\\|/";
+    int index = 0;
+    while (isCalculating) {
+        std::cout << "\rCalculating... " << indicators[index++];
+        index %= indicators.size();
+        std::cout.flush();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+    std::cout << '\n';
+}
+
+template <typename T>
+auto measureTime(const std::vector<T>& a, const std::vector<T>& b,
+                 std::vector<T>& result, const std::string& operationType) {
+    auto start = std::chrono::high_resolution_clock::now();
+    if (operationType == "p") {
+        matrixMul(a, b, result);
+    } else if (operationType == "np") {
+        matrixMulNotParallel(a, b, result);
+    } else {
+        throw std::invalid_argument("Invalid operationType argument!");
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed_time =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    return elapsed_time.count();
+}
+
+template <typename T>
+void calculate(const std::vector<std::filesystem::path>& filePaths,
+               const std::filesystem::path& outFileName,
+               const std::string& operationType) {
+    std::fstream outfile(outFileName, std::ios::trunc | std::ios::out);
+    if (!outfile.is_open()) {
+        throw std::invalid_argument("Invalid outFileName! " +
+                                    outFileName.string());
+    }
+    std::atomic<bool> isCalculating;
+    isCalculating.store(true);
+    std::thread progressThread(printProgressIndicator, std::ref(isCalculating));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    for (const auto& filePath : filePaths) {
+        std::vector<T> a;
+        readFromBinary(a, filePath);
+        std::vector<T> b = a;
+        std::vector<T> result(a.size(), 0);
+        auto time = measureTime<T>(a, b, result, operationType);
+        outfile << std::to_string(time) + "    " +
+                       filePath.filename().string() + "\n";
+    }
+    outfile.close();
+    isCalculating.store(false);
+    progressThread.join();
+}
+
+int main(int argc, char* argv[]) {
+    omp_set_num_threads(omp_get_max_threads());
+    std::filesystem::path outFileName = std::filesystem::absolute(argv[1]);
+    std::string dataDir = argv[2];
+    std::string operationType = argv[3];
+    if (operationType != "p" && operationType != "np") {
+        std::cout << "3rd argument nust be equal to p (parallel) or np (not "
+                     "parallel)!";
+        return 0;
+    }
+    std::filesystem::directory_iterator iterator(dataDir);
+    std::vector<std::filesystem::path> fileNames;
+    for (const auto& entry : iterator) {
+        if (entry.is_regular_file()) {
+            std::filesystem::path filePath =
+                std::filesystem::absolute(entry.path()).lexically_normal();
+            fileNames.push_back(filePath);
+        }
+    }
+    calculate<float>(fileNames, outFileName, operationType);
+}
---
+++ src/GPU.cu
@@ -0,0 +1,183 @@
+﻿#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
+#include <atomic>
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <thread>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+template <typename T>
+__global__ void matrixMulKernel(T* c, const T* a, const T* b, size_t N) {
+    size_t j = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t i = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < N && j < N) {
+        auto idx = i * N + j;
+        for (size_t k = 0; k < N; k++) {
+            c[idx] += a[i * N + k] * b[k * N + j];
+        }
+    }
+}
+
+template <typename T>
+__global__ void matrixMulKernelNotParallel(T* c, const T* a, const T* b,
+                                           size_t N) {
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < N; k++) {
+            for (int j = 0; j < N; j++) {
+                c[i * N + j] += a[i * N + k] * b[k * N + j];
+            }
+        }
+    }
+}
+
+#define BLOCK_DIM 16
+template <typename T>
+__global__ void matrixMulKernelSharedMemory(T* c, const T* a, const T* b,
+                                            const size_t N) {
+    int blockX = blockIdx.x;
+    int blockY = blockIdx.y;
+    int threadX = threadIdx.x;
+    int threadY = threadIdx.y;
+    int i = blockY * blockDim.y + threadY;
+    int j = blockX * blockDim.x + threadX;
+    __shared__ T aShared[BLOCK_DIM][BLOCK_DIM];
+    __shared__ T bShared[BLOCK_DIM][BLOCK_DIM];
+    T sum = 0;
+    for (int part = 0; part < ceil(static_cast<T>(N) / BLOCK_DIM); ++part) {
+        int iRow = i;
+        int iCol = part * BLOCK_DIM + threadX;
+        int jCol = j;
+        int jRow = part * BLOCK_DIM + threadY;
+
+        aShared[threadY][threadX] =
+            (iRow < N && iCol < N) ? a[iRow * N + iCol] : 0;
+        bShared[threadY][threadX] =
+            (jRow < N && jCol < N) ? b[jRow * N + jCol] : 0;
+
+        __syncthreads();
+        for (int idx = 0; idx < BLOCK_DIM; ++idx) {
+            sum += aShared[threadY][idx] * bShared[idx][threadX];
+        }
+        __syncthreads();
+    }
+    if (i < N && j < N) {
+        c[i * N + j] = sum;
+    }
+}
+
+template <typename T>
+void readFromBinary(thrust::host_vector<T>& v,
+                    const std::filesystem::path& fileName) {
+    std::ifstream file(fileName, std::ios::binary);
+    if (!file.good())
+        throw std::invalid_argument("Invalid file! " + fileName.string());
+    size_t size = 0;
+    file.read(reinterpret_cast<char*>(&size), sizeof(size_t));
+    v.resize(size);
+    file.read(reinterpret_cast<char*>(v.data()), size * sizeof(T));
+    file.close();
+}
+
+void printProgressIndicator(std::atomic<bool>& isCalculating) {
+    const std::string indicators = "-\\|/";
+    int index = 0;
+    while (isCalculating) {
+        std::cout << "\rCalculating... " << indicators[index++];
+        index %= indicators.size();
+        std::cout.flush();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+    std::cout << '\n';
+}
+
+template <typename T>
+long long measureTime(const thrust::device_vector<T>& a,
+                      thrust::device_vector<T>& b,
+                      thrust::device_vector<T>& result, size_t N,
+                      const std::string& kernelType) {
+    dim3 blockSize;
+    dim3 numBlocks;
+    if (kernelType == "ps" || kernelType == "p") {
+        blockSize = dim3(BLOCK_DIM, BLOCK_DIM);
+        numBlocks = dim3((N + blockSize.x - 1) / blockSize.x,
+                         (N + blockSize.y - 1) / blockSize.y);
+    } else {
+        blockSize = dim3(1, 1);
+        numBlocks = dim3(1, 1);
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+    auto rawResult = thrust::raw_pointer_cast(result.data());
+    auto rawA = thrust::raw_pointer_cast(a.data());
+    auto rawB = thrust::raw_pointer_cast(b.data());
+    if (kernelType == "p") {
+        matrixMulKernel<<<numBlocks, blockSize>>>(rawResult, rawA, rawB, N);
+    } else if (kernelType == "np") {
+        matrixMulKernelNotParallel<<<numBlocks, blockSize>>>(rawResult, rawA,
+                                                             rawB, N);
+    } else if (kernelType == "ps") {
+        matrixMulKernelSharedMemory<<<numBlocks, blockSize>>>(rawResult, rawA,
+                                                              rawB, N);
+    } else {
+        throw std::invalid_argument("Invalid kernelType argument!");
+    }
+    cudaDeviceSynchronize();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed_time =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    return elapsed_time.count();
+}
+
+template <typename T>
+void calculate(const std::vector<std::filesystem::path>& filePaths,
+               const std::filesystem::path& outFileName,
+               const std::string& kernelType) {
+    std::fstream outfile(outFileName, std::ios::trunc | std::ios::out);
+    if (!outfile.is_open()) {
+        throw std::invalid_argument("Invalid outFileName! " +
+                                    outFileName.string());
+    }
+    std::atomic<bool> isCalculating;
+    isCalculating.store(true);
+    std::thread progressThread(printProgressIndicator, std::ref(isCalculating));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    for (const auto& filePath : filePaths) {
+        thrust::host_vector<T> h_a;
+        readFromBinary<T>(h_a, filePath);
+        thrust::device_vector<T> d_a = h_a;
+        thrust::device_vector<T> d_b = h_a;
+        thrust::device_vector<T> d_c(h_a.size());
+        const size_t N = sqrt(h_a.size());
+        auto time = measureTime<T>(d_a, d_b, d_c, N, kernelType);
+        outfile << std::to_string(time) + "    " +
+                       filePath.filename().string() + "\n";
+    }
+    outfile.close();
+    isCalculating.store(false);
+    progressThread.join();
+}
+
+int main(int argc, char* argv[]) {
+    std::filesystem::path outFileName = std::filesystem::absolute(argv[1]);
+    std::string dataDir = argv[2];
+    std::string kernelType = argv[3];
+    if (kernelType != "p" && kernelType != "ps" && kernelType != "np") {
+        std::cout << "3rd argument must be equal to p (parallel) or ps "
+                     "(parallel with shared memory) or np (not parallel)!";
+        return 0;
+    }
+    std::filesystem::directory_iterator iterator(dataDir);
+    std::vector<std::filesystem::path> fileNames;
+    for (const auto& entry : iterator) {
+        if (entry.is_regular_file()) {
+            std::filesystem::path filePath =
+                std::filesystem::absolute(entry.path()).lexically_normal();
+            fileNames.push_back(filePath);
+        }
+    }
+    calculate<float>(fileNames, outFileName, kernelType);
+    return 0;
+}
